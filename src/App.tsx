df81f89360547faa4d1e6256a576682d026781fc
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-02 14:45:34 +0100

    Ajouter :  structer Du code avec install Redux/toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Sidebar from "./layouts/Sidebar.tsx";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
             <h1 className="text-3xl font-bold underline">
                 Chat APP React
             </h1>
+            <Sidebar/>
         </>
     )
 }
