1cc4a106c313395610ee73cbfe7ec7ba28e2d166
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-11 15:38:43 +0100

    Component: Add Sidebar component in layouts folder

--- src/pages/layouts/Sidebar.tsx
+++ src/pages/layouts/Sidebar.tsx
@@ -1,9 +1,43 @@
+// Sidebar.tsx
+import React from 'react';
+import {Badge} from '@mui/material';
+import {AccountCircleOutlined, ChatBubble, Dashboard, HomeOutlined, LogoutOutlined, PeopleOutlined} from '@mui/icons-material';
 
-function Sidebar() {
+const Sidebar: React.FC = () => {
     return (
-        <>
-        </>
+        <div
+            className="h-screen w-16 bg-white shadow-lg flex flex-col items-center py-4 border border-blue-300 rounded-xl">
+            <div className="mb-6">
+                <Dashboard fontSize="large" className="text-purple-500"/>
+            </div>
+
+            <div className="mb-6 cursor-pointer">
+                <HomeOutlined fontSize="large" className="text-purple-500"/>
+            </div>
+
+            <div className="mb-6 cursor-pointer bg-purple-100 rounded-full p-2">
+                <ChatBubble fontSize="large" className="text-purple-500"/>
+            </div>
+
+            <div className="mb-6 cursor-pointer relative">
+                <Badge badgeContent={2} color="error" overlap="circular">
+                    <PeopleOutlined fontSize="large" className="text-purple-500"/>
+                </Badge>
+            </div>
+
+            <div className="flex-grow border-b border-dashed border-blue-300 w-full"></div>
+
+            <div className="flex flex-col items-center mt-4 space-y-6">
+                <AccountCircleOutlined
+                    fontSize="large" className="text-purple-500"
+                />
+
+                <div className="cursor-pointer">
+                    <LogoutOutlined fontSize="large" className="text-purple-500"/>
+                </div>
+            </div>
+        </div>
     );
-}
+};
 
 export default Sidebar;
