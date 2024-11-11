b4d3a66e6fabd005aef9f8d405097167f2e5eea3
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-11 15:31:51 +0100

    Components: Import Sidebar and Layout in index and create Sidebar component

--- src/pages/layouts/Layout.tsx
+++ src/pages/layouts/Layout.tsx
@@ -1,7 +1,19 @@
+import {Sidebar} from "./index.ts";
 
 function Layout() {
     return (
         <>
+            <header>
+                <Sidebar />
+            </header>
+            <main>
+                <div className="container">
+                    <div className="chat-box">
+
+                    </div>
+                </div>
+            </main>
+
         </>
     );
 }
