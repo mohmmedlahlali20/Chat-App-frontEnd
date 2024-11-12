d2163f2fb6514b12f11a9ed3a4e948650e5779f1
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-12 16:03:24 +0100

    Add : video Reducer in store

--- src/Redux/Store/store.tsx
+++ src/Redux/Store/store.tsx
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import channelReducer from '../Slices/Channel/ChannelSlice';
+import videoCallReducer from '../Slices/videoCallSlice/videoCallSlice'
 import { channelApi } from '../../services/channelApi.tsx';
 import { setupListeners } from '@reduxjs/toolkit/query';
 
 const store = configureStore({
     reducer: {
         channel: channelReducer,
+        videoCall: videoCallReducer,
         [channelApi.reducerPath]: channelApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
