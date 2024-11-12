4888e8ddc44b6febc9157c8b9b107c8715090deb
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-12 20:58:50 +0100

    Fix : Chating

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,20 @@
 import Channel from "./channels/Channel.tsx";
 import CreateChannel from "./channels/CreateChannel.tsx";
 import ChatBox from "./Chats/ChatBox.tsx"
+import AllChannels from "./channels/AllChannels.tsx";
+import InputSearch from "./inputSearch.tsx";
+import ChannelList from "./channelList.tsx";
+import FriendList from "./friendList.tsx";
+import FriendDetails from "./friendDetails.tsx";
 
 
 export {
     Channel,
     CreateChannel,
-    ChatBox
+    ChatBox,
+    AllChannels,
+    InputSearch,
+    ChannelList,
+    FriendList,
+    FriendDetails
 }
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,7 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "../pages/layouts";
 import { NotFound } from "../pages";
 import {ChatBox} from "../components";
-
+import { Home } from "../pages";
+ 
 const router = createBrowserRouter([
     {
         element: <Layout />,
@@ -11,12 +12,18 @@ const router = createBrowserRouter([
                 path: "/",
                 element: <ChatBox />
             },
-        ]
+          
+        ]   
     },
     {
         path: "*",
         element: <NotFound />
-    }
+    },
+    {
+        path: "/Home",
+        element: <Home />
+    },
+    
 ]);
 
 export default router;
