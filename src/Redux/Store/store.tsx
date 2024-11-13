3497dee54e117e77d7bdd5ebc3c96e25a09ae7b0
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-13 16:44:46 +0100

    Add: create store for user management

--- src/Redux/Store/store.tsx
+++ src/Redux/Store/store.tsx
@@ -2,14 +2,18 @@ import { configureStore } from '@reduxjs/toolkit';
 import channelReducer from '../Slices/Channel/ChannelSlice';
 import { channelApi } from '../../services/channelApi.tsx';
 import { setupListeners } from '@reduxjs/toolkit/query';
-
+import usersReducer from '../Slices/users/userSlice';
+import { userApi } from '../../services/userApi';
 const store = configureStore({
     reducer: {
         channel: channelReducer,
+        users: usersReducer,
         [channelApi.reducerPath]: channelApi.reducer,
+        [userApi.reducerPath]: userApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(channelApi.middleware),
+        getDefaultMiddleware().concat(channelApi.middleware,userApi.middleware),
+
 });
 
 setupListeners(store.dispatch);
