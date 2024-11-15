d0bf5574d9dd4d7a58a102abe137f4757cd61af4
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-15 12:17:04 +0100

    ADD : create Slice in redux folder  for message with send message features addMessage and Remov it

---
+++ src/Redux/Slices/messages/MessageSlice.ts
@@ -0,0 +1,47 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+interface Message {
+    _id: string;
+    sender: string;
+    text: string;
+    createdAt: Date;
+    channel: string;
+    updateAt: Date;
+}
+
+interface MessageState {
+    messages: Message[];
+}
+
+const initialState: MessageState = {
+    messages: [],
+};
+
+const messageSlice = createSlice({
+    name: "message",
+    initialState,
+    reducers: {
+        sendMessage: (state, action: PayloadAction<Message>) => {
+            state.messages.push(action.payload)
+        },
+
+        removeMessage: (state, action: PayloadAction<{ _id: string; sender: string }>) => {
+            const { _id, sender } = action.payload;
+            state.messages = state.messages.filter(
+                (msg) => msg._id !== _id || msg.sender !== sender
+            );
+        }
+
+
+
+    },
+});
+
+export const {
+    sendMessage,
+    removeMessage
+} = messageSlice.actions
+
+
+export default messageSlice.reducer
+
